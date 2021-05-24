cf89b6102b26b44a1c6ada8f6376dd4625654711
Rastro1994 <423@mail.ru>
2021-05-24 22:53:01 +0500

    Добавил .gitignore+все введенные значения

--- src/test/java/DemoQATest.java
+++ src/test/java/DemoQATest.java
@@ -11,6 +11,13 @@ public class DemoQATest {
             lastName = "Ivanov",
             email = "43fsad423eds@mail.ru",
             number = "6464614684",
+            month = "May",
+            year = "1994",
+            day = "28",
+            subject = "Hindi",
+            hobbie1 = "Sports",
+            hobbie2 = "Reading",
+            picture = "autist-472x312_c.jpg",
             address = "45648das";
 
     @Test
@@ -22,14 +29,14 @@ public class DemoQATest {
         $(byText("Male")).click();
         $("#userNumber").setValue(number);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("May");
-        $(".react-datepicker__year-select").selectOption("1994");
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day--028").click();
-        $("#subjectsInput").setValue("Hindi");
+        $("#subjectsInput").setValue(subject);
         $("#react-select-2-option-0").click();
-        $(byText("Sports")).click();
-        $(byText("Reading")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/upload/" + "autist-472x312_c.jpg"));
+        $(byText(hobbie1)).click();
+        $(byText(hobbie2)).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/upload/" + picture));
         $("#currentAddress").setValue(address);
         $("#currentAddress").scrollTo();
         $("#state").click();
@@ -37,7 +44,20 @@ public class DemoQATest {
         $("#city").click();
         $("#react-select-4-option-0").click();
         $("#submit").click();
-        $(".modal-content").shouldHave(text(firstName), text(lastName), text(email), text(number), text(address));
+        $(".modal-content").shouldHave(
+                text(firstName),
+                text(lastName),
+                text(email),
+                text(number),
+                text(address),
+                text(month),
+                text(year),
+                text(day),
+                text(subject),
+                text(hobbie1),
+                text(hobbie2),
+                text(picture),
+                text(address));
 
     }
 }

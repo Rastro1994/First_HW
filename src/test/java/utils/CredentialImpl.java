659ca3e8668023f7d230bfd60c749cf6bffd8612
Rastro1994 <423@mail.ru>
2021-06-23 00:27:46 +0500

    Jenkins Video Recording Test

--- src/test/java/utils/CredentialImpl.java
+++ src/test/java/utils/CredentialImpl.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/creds.properties"
+        "classpath:config/login.properties"
 })
 public interface CredentialImpl extends Config {
 
